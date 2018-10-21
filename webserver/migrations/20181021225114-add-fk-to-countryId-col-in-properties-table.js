a8e7f038c13e8adb537d7603dc7e994a959248de
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 00:20:54 +0100

    added mysql migration file that add a FK to the countryId column of properties table

---
+++ webserver/migrations/20181021225114-add-fk-to-countryId-col-in-properties-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return(db.runSql(
+    `ALTER TABLE properties
+    ADD CONSTRAINT FOREIGN KEY fk_properties_countryId (countryId) REFERENCES countries(id) ON DELETE CASCADE ON UPDATE CASCADE`
+  ));
+};
+
+exports.down = function(db) {
+  return(db.runSql(
+    `ALTER TABLE properties
+    DROP FOREIGN KEY IF EXISTS fk_properties_countryId`
+  ));
+};
+
+exports._meta = {
+  "version": 1
+};
