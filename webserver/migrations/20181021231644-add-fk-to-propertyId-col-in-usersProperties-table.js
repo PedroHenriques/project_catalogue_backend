6b8d1c04f33549779c245e134d6749227bc68234
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 00:22:09 +0100

    added mysql migration file that adds a FK to propertyId column of usersProperties tables

---
+++ webserver/migrations/20181021231644-add-fk-to-propertyId-col-in-usersProperties-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return(db.runSql(
+    `ALTER TABLE usersProperties
+    ADD CONSTRAINT FOREIGN KEY fk_usersProperties_propertyId (propertyId) REFERENCES properties(id) ON DELETE CASCADE ON UPDATE CASCADE`
+  ));
+};
+
+exports.down = function(db) {
+  return(db.runSql(
+    `ALTER TABLE usersProperties
+    DROP FOREIGN KEY IF EXISTS fk_usersProperties_propertyId`
+  ));
+};
+
+exports._meta = {
+  "version": 1
+};
