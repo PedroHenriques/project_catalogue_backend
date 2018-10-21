28c5d8e9c6e9650cd0a100e0bc41c1bdc52b228a
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 00:20:08 +0100

    added mysql migration file that creates the 'countries' table

---
+++ webserver/migrations/20181021224859-create-countries-table.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return(db.runSql(
+    `CREATE TABLE IF NOT EXISTS countries
+    (
+      id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL
+    ) ENGINE=InnoDB`
+  ));
+};
+
+exports.down = function(db) {
+  return(db.runSql(
+    `DROP TABLE IF EXISTS countries`
+  ));
+};
+
+exports._meta = {
+  "version": 1
+};
