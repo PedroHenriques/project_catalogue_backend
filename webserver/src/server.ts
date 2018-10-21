7b3b97d57465e2f85d2728e32cec127f567a133b
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-21 16:59:20 +0100

    added the entry point file for the webserver container

---
+++ webserver/src/server.ts
@@ -0,0 +1,17 @@
+'use strict';
+import * as express from 'express';
+import logger from './services/Logger';
+
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = 'production';
+}
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  logger.info({
+    message: `The server is running in "${process.env.NODE_ENV}" mode`
+  });
+  logger.info({ message: `Listening on localhost:${PORT}` });
+});
