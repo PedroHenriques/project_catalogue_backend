ce758648fc13b45bb083084c2919519fc010dd28
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 17:52:41 +0100

    added method to the Property handler to create a new property for the logged in user

--- webserver/src/handlers/PropertyHandler.ts
+++ webserver/src/handlers/PropertyHandler.ts
@@ -40,4 +40,76 @@ export default class PropertyHandler {
       }));
     }
   }
+
+  public create = async (req: Request, res: Response): Promise<Response> => {
+    let dbConnection: Connection | undefined;
+    try {
+      const userId = '1';
+
+      dbConnection = await SqlFacade.connect();
+      await new Promise((resolve, reject) => {
+        if (dbConnection === undefined) {
+          return(reject(new Error('Failed to connect to the DB')));
+        }
+
+        dbConnection.beginTransaction(async error => {
+          if (error) { return(reject(error)); }
+          if (dbConnection === undefined) { return(reject()); }
+
+          try {
+            const property: IPropertyTable = {
+              title: req.body.title,
+              numberOfBeds: parseInt(req.body.numberOfBeds, 10),
+              address: req.body.address,
+              geoLocation: parseFloat(req.body.geoLocation),
+              description: (req.body.description ? req.body.description : null),
+              typeId: parseInt(req.body.typeId, 10),
+              countryId: parseInt(req.body.countryId, 10),
+            };
+
+            const insertedProperty = await SqlFacade.query(
+              dbConnection,
+              {
+                statement: `INSERT INTO properties SET ?`,
+                bindValues: [ property ],
+              }
+            );
+
+            const userProperty: IUsersPropertiesTable = {
+              userId: parseInt(userId, 10),
+              propertyId: insertedProperty.insertId,
+            };
+
+            await SqlFacade.query(
+              dbConnection,
+              {
+                statement: 'INSERT INTO usersProperties SET ?',
+                bindValues: [ userProperty ],
+              }
+            );
+
+            dbConnection.commit();
+
+            resolve();
+          } catch (error) {
+            dbConnection.rollback();
+            reject(error);
+          }
+        });
+      });
+
+      return(res.status(201).json({}));
+    } catch (error) {
+      logger.error({
+        message: error.message,
+        payload: error,
+      });
+
+      return(res.status(500).json({
+        error: 'Could not handle the request.',
+      }));
+    } finally {
+      if (dbConnection) { dbConnection.end(); }
+    }
+  }
 }
