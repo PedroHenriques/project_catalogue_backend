53d7cb2eb4ac6a04fb871111ea9db7aa874d4248
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 14:29:10 +0100

    added migration files with temporary DB seed data, to be used during development

---
+++ webserver/migrations/20181022123616-add-properties-seeds.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return(db.runSql(
+    `INSERT INTO properties (title, numberOfBeds, address,geoLocation,description,typeId,countryId)
+    VALUES ('property 1',2,'address 1',30.456,'this is property 1',1,1),
+    ('property 2',3,'address 2',10.2,null,2,2)`
+  ));
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
