b1f0b3cd5d8a9ff5af7ab93a128610734716a542
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 14:47:37 +0100

    updated the test client application to have the GET request for the Property resource endpoint

--- webserver/client_app.js
+++ webserver/client_app.js
@@ -3,3 +3,20 @@ const fetch = require('node-fetch');
 
 const serverURL = 'http://localhost:8000/';
 
+module.exports.getAllProperties = () => {
+  fetch(`${serverURL}api/v1/properties`, {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.error) { throw data.error; }
+
+    console.log(data.properties);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
