0fa0d6f2e368fb1f291b3dde83765111a90fe90f
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 15:25:27 +0100

    added try-catch block to handler method

--- webserver/src/handlers/PropertyHandler.ts
+++ webserver/src/handlers/PropertyHandler.ts
@@ -5,18 +5,29 @@ import { IProperty } from '../interfaces/data';
 
 export default class PropertyHandler {
   public findAll = async (req: Request, res: Response): Promise<Response> => {
-    const userId = '1';
+    try {
+      const userId = '1';
 
-    const properties = await runQuery({
-      statement: `SELECT p.title, p.numberOfBeds, p.address, p.geoLocation,
-        p.description, pt.name as propertyType, c.name as country
-        FROM properties as p
-        LEFT JOIN usersProperties as up ON up.propertyId=p.id AND up.userId = ?
-        LEFT JOIN propertyTypes as pt ON pt.id = p.typeId
-        LEFT JOIN countries as c ON c.id = p.countryId`,
-      bindValues: [ userId ],
-    }) as IProperty[];
+      const properties = await runQuery({
+        statement: `SELECT p.title, p.numberOfBeds, p.address, p.geoLocation,
+          p.description, pt.name as propertyType, c.name as country
+          FROM properties as p
+          LEFT JOIN usersProperties as up ON up.propertyId=p.id AND up.userId = ?
+          LEFT JOIN propertyTypes as pt ON pt.id = p.typeId
+          LEFT JOIN countries as c ON c.id = p.countryId`,
+        bindValues: [ userId ],
+      }) as IProperty[];
 
-    return(res.status(200).json({ properties }));
+      return(res.status(200).json({ properties }));
+    } catch (error) {
+      logger.error({
+        message: error.message,
+        payload: error,
+      });
+
+      return(res.status(500).json({
+        error: 'Could not handle the request.',
+      }));
+    }
   }
 }
