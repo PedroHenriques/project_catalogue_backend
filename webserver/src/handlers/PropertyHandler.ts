01668e486ea9e575341ac4c0a5e21f96c6d4ee02
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 14:37:53 +0100

    added imports

--- webserver/src/handlers/PropertyHandler.ts
+++ webserver/src/handlers/PropertyHandler.ts
@@ -1,3 +1,6 @@
 'use strict';
+import { Request, Response } from 'express';
+import { runQuery } from '../services/Mysql';
+import { IProperty } from '../interfaces/data';
 
 export default class PropertyHandler {}
