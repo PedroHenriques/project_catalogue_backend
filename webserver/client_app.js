05c0ee00741f98e4b11a9532cfb11c6eb9928603
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 18:50:34 +0100

    updated the test client application to get a user's properties

--- webserver/client_app.js
+++ webserver/client_app.js
@@ -21,6 +21,24 @@ module.exports.getAllProperties = () => {
   });
 }
 
+module.exports.getMyProperties = () => {
+  fetch(`${serverURL}api/v1/users/properties`, {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.error) { throw data.error; }
+
+    console.log(data.properties);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
 module.exports.createProperty = (
   title, numberOfBeds, address, geoLocation, description, typeId, countryId
 ) => {
