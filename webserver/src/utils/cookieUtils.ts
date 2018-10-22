965165dc43f03db4f7bcca1df2205150aa58810e
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 23:25:33 +0100

    added util module with cookie related util functions

---
+++ webserver/src/utils/cookieUtils.ts
@@ -0,0 +1,35 @@
+'use strict';
+import { Request, CookieOptions, Response } from 'express';
+
+export const getCookie = (
+  args: { req: Request, name: string }
+): string | null => {
+  if (
+    args.req.cookies === undefined ||
+    args.req.cookies[args.name] === undefined
+  ) {
+    return(null);
+  }
+
+  return(args.req.cookies[args.name]);
+};
+
+export const setCookie = (
+  args: { res: Response, name: string, value: string }
+): Response => args.res.cookie(args.name, args.value, cookieOptions());
+
+export const clearCookie = (
+  args: { res: Response, name: string }
+): Response => args.res.clearCookie(args.name, cookieOptions());
+
+const cookieOptions = (): CookieOptions => {
+  const options: CookieOptions = {
+    httpOnly: false,
+  };
+
+  if (process.env.NODE_ENV !== 'development') {
+    options.secure = true;
+  }
+
+  return(options);
+};
