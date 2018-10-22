2ea31ac951c3c2d74c4704aa0cee74c6d9ae5b0b
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-22 14:32:33 +0100

    added function to the Mysql service that executes a query on the SQL databse

--- webserver/src/services/Mysql.ts
+++ webserver/src/services/Mysql.ts
@@ -1,5 +1,40 @@
 'use strict';
 import { createConnection, Connection } from 'mysql';
+import logger from '../services/Logger';
+import { IProperty } from '../interfaces/data';
+
+interface IQuery {
+  statement: string,
+  bindValues?: (string | number | boolean | Date)[]
+}
+
+export function runQuery(args: IQuery): Promise<IProperty[]> {
+  return(
+    connect()
+    .then(async (connection) => new Promise((resolve, reject) => {
+      connection.query(
+        args.statement,
+        args.bindValues,
+        (error, results, fields) => {
+          connection.end();
+          if (error) { return(reject(error)); }
+          return(resolve(results));
+        }
+      );
+    }))
+    .then(result => {
+      return(result as IProperty[]);
+    })
+    .catch(error => {
+      logger.error({
+        message: error.message,
+        payload: error,
+      });
+
+      return([]);
+    })
+  );
+}
 
 function connect(): Promise<Connection> {
   return(new Promise((resolve, reject) => {
