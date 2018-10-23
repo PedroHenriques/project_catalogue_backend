f706b4e5e254b71414d30def9dc3a0b684959f78
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 13:26:54 +0100

    added imports necessary for session handling

--- webserver/src/routers/v1.ts
+++ webserver/src/routers/v1.ts
@@ -1,5 +1,7 @@
 'use strict';
 import { Router } from 'express';
+import * as cookieParser from 'cookie-parser';
+import session from '../middleware/session';
 import PropertyHandler from '../handlers/PropertyHandler';
 
 const router = Router();
