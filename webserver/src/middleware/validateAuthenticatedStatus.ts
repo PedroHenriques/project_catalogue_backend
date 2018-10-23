3ab720cb8f5bb402754a18cd6bc0f4fcdadea577
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 13:24:42 +0100

    added expressJS middleware to allow blocking access to routes based on the authenticated status of a user

---
+++ webserver/src/middleware/validateAuthenticatedStatus.ts
@@ -0,0 +1,33 @@
+'use strict';
+import { NextFunction, Request, Response } from 'express';
+import logger from '../services/Logger';
+
+export default function validateAuthenticatedStatus(
+  denyAccessIf: 'loggedIn' | 'notLoggedIn'
+) {
+  return(
+    async (
+      req: Request, res: Response, next: NextFunction
+    ): Promise<void | Response> => {
+      logger.debug({ message: 'Starting logged in verification' });
+
+      const isLoggedIn: boolean = req.session !== undefined;
+      if (isLoggedIn && denyAccessIf === 'loggedIn') {
+        logger.debug({ message: 'Denied because is logged in' });
+
+        return(res.status(403).json({
+          error: 'The request is only valid for non authenticated users',
+        }));
+      }
+      if (!isLoggedIn && denyAccessIf === 'notLoggedIn') {
+        logger.debug({ message: 'Denied because is not logged in' });
+
+        return(res.status(401).json({
+          error: 'Invalid credentials',
+        }));
+      }
+
+      next();
+    }
+  );
+}
