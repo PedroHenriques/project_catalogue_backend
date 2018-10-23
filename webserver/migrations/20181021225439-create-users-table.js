d14e8692ac7df7321e9b9863eee797b9a6800c6a
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 12:53:18 +0100

    updated migration that created the users table to add a UNIQUE index to the email column

--- webserver/migrations/20181021225439-create-users-table.js
+++ webserver/migrations/20181021225439-create-users-table.js
@@ -21,7 +21,8 @@ exports.up = function(db) {
       id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
       email VARCHAR(100) NOT NULL,
       password VARCHAR(255) NOT NULL,
-      name VARCHAR(255) NOT NULL
+      name VARCHAR(255) NOT NULL,
+      UNIQUE KEY (email)
     ) ENGINE=InnoDB`
   ));
 };
