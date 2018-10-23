d18f1924eeba126ebd143953761fe2da9c6377ac
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 12:53:48 +0100

    updated migration that created the users table to have created and last modified timestamps columns

--- webserver/migrations/20181021225439-create-users-table.js
+++ webserver/migrations/20181021225439-create-users-table.js
@@ -22,6 +22,8 @@ exports.up = function(db) {
       email VARCHAR(100) NOT NULL,
       password VARCHAR(255) NOT NULL,
       name VARCHAR(255) NOT NULL,
+      createdAt DATETIME NOT NULL DEFAULT NOW(),
+      updatedAt DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW(),
       UNIQUE KEY (email)
     ) ENGINE=InnoDB`
   ));
